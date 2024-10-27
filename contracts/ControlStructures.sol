bef8cb5f70f418b640906a0e27c02125a7e59955
eGucio <egucio70@gmail.com>
2024-10-27 21:41:35 +0100

    loop added

--- contracts/ControlStructures.sol
+++ contracts/ControlStructures.sol
@@ -1,12 +1,32 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
+import "hardhat/console.sol";
+
 contract ControlStructures {
 
     function compareNumbers(uint numberOne, uint numberTwo) external pure returns (string memory) {
         if(numberOne>numberTwo) return "Number one is greater than number two";
         else if(numberOne<numberTwo) return "Number one is less than number two";
         else return "Number one equals number two";
+    }
+
+    function loop() external pure{ 
+        for(uint index=0; index<8;index++){
+            if(index==2){
+                console.log("index is ", index);
+                continue ;
+            }
+
+            console.log("After index ==2");
+
+            if(index==4){
+                console.log("index is ", index);
+                break;
+            }
+
+            console.log("End of the loop");
+        }
     }
     
 }
