bf203dff93209e938da17b57cda0e2b25a85abbc
eGucio <egucio70@gmail.com>
2024-10-27 22:32:14 +0100

    added Require, thenCustomError InvalidData and Revert

--- contracts/ControlStructures.sol
+++ contracts/ControlStructures.sol
@@ -1,32 +1,29 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
-
 import "hardhat/console.sol";
-
 contract ControlStructures {
-
+    error InvalidData();
     function compareNumbers(uint numberOne, uint numberTwo) external pure returns (string memory) {
+        //require(numberOne > 0 && numberTwo > 0, "Invalid data");
+        if(numberOne<=0 || numberTwo<=0){
+            revert InvalidData();
+        }
         if(numberOne>numberTwo) return "Number one is greater than number two";
         else if(numberOne<numberTwo) return "Number one is less than number two";
         else return "Number one equals number two";
     }
-
     function loop() external pure{ 
         for(uint index=0; index<8;index++){
             if(index==2){
                 console.log("index is ", index);
                 continue ;
             }
-
             console.log("After index ==2");
-
             if(index==4){
                 console.log("index is ", index);
                 break;
             }
-
             console.log("End of the loop");
         }
     }
-    
 }
