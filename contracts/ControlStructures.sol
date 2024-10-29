6afb8e35e745c0f7d47e2aa4b6e5f51fdf9735b1
eGucio <egucio70@gmail.com>
2024-10-29 16:18:35 +0100

    Control Structures Exercise

--- contracts/ControlStructures.sol
+++ contracts/ControlStructures.sol
@@ -3,6 +3,8 @@ pragma solidity ^0.8.17;
 import "hardhat/console.sol";
 contract ControlStructures {
     error InvalidData();
+    error AfterHours(uint time);
+
     function compareNumbers(uint numberOne, uint numberTwo) external pure returns (string memory) {
         //require(numberOne > 0 && numberTwo > 0, "Invalid data");
         if(numberOne<=0 || numberTwo<=0){
@@ -12,6 +14,7 @@ contract ControlStructures {
         else if(numberOne<numberTwo) return "Number one is less than number two";
         else return "Number one equals number two";
     }
+
     function loop() external pure{ 
         for(uint index=0; index<8;index++){
             if(index==2){
@@ -25,5 +28,22 @@ contract ControlStructures {
             }
             console.log("End of the loop");
         }
+    }
+
+    function fizzBuzz(uint _number) external pure returns(string memory){
+        if(_number % 3 == 0 && _number % 5 == 0){return"FizzBuzz";}
+        else if(_number % 3 == 0){return"Fizz";}
+        else if(_number % 5 == 0){return"Buzz";}
+        else return "Splat";
+    }
+
+    function doNotDisturb(uint _time) external pure returns(string memory) {
+        if(_time > 2400) assert(false);
+        else if(_time > 2200 || _time < 800) revert AfterHours(_time);
+        else if(_time >= 1200 && _time <= 1259) revert("At lunch!");
+        else if(_time >= 1800){return "Evening!";}
+        else if(_time >= 1300){return "Afternoon!";}
+        else return "Morning!";
+        return "Morning!";
     }
 }
